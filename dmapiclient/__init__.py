2450df490e8ff71fa3faf447ba52b06ed35f408c
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2016-03-30 13:09:04 +0100

    Bump version to 3.7.0

--- dmapiclient/__init__.py
+++ dmapiclient/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '3.6.0'
+__version__ = '3.7.0'
 
 from .errors import APIError, HTTPError, InvalidResponse  # noqa
 from .errors import REQUEST_ERROR_STATUS_CODE, REQUEST_ERROR_MESSAGE  # noqa
