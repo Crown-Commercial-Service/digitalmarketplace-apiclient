049ff77da17cbd6116493dbb81bb8fcc9c009048
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2016-08-22 09:22:11 +0100

    Bump version to 6.3.1

--- dmapiclient/__init__.py
+++ dmapiclient/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '6.3.0'
+__version__ = '6.3.1'
 
 from .errors import APIError, HTTPError, InvalidResponse  # noqa
 from .errors import REQUEST_ERROR_STATUS_CODE, REQUEST_ERROR_MESSAGE  # noqa
