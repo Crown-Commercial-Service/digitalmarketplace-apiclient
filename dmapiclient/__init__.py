cb01629e8b45358004754176e48cbf81c9d66386
Paul Craig <paul_craig_16@hotmail.com>
2016-09-02 16:05:25 +0100

    Bump version to 6.3.4

--- dmapiclient/__init__.py
+++ dmapiclient/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '6.3.3'
+__version__ = '6.4.0'
 
 from .errors import APIError, HTTPError, InvalidResponse  # noqa
 from .errors import REQUEST_ERROR_STATUS_CODE, REQUEST_ERROR_MESSAGE  # noqa
