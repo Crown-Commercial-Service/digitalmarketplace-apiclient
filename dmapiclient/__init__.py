13fdebc1a737c71c72d0ed42c1161f283df8736d
Paul Craig <paul_craig_16@hotmail.com>
2017-06-28 11:26:22 +0100

    Bump version to 8.10.2

--- dmapiclient/__init__.py
+++ dmapiclient/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '8.10.1'
+__version__ = '8.10.2'
 
 from .errors import APIError, HTTPError, InvalidResponse  # noqa
 from .errors import REQUEST_ERROR_STATUS_CODE, REQUEST_ERROR_MESSAGE  # noqa
