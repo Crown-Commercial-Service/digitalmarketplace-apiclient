596082be29f18a249092404331916593f454e0db
Samuel Williams <Samuel.Williams@digital.cabinet-office.gov.uk>
2017-09-18 07:42:39 +0100

    Bump version to 10.7.0

--- dmapiclient/__init__.py
+++ dmapiclient/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '10.6.0'
+__version__ = '10.7.0'
 
 from .errors import APIError, HTTPError, InvalidResponse  # noqa
 from .errors import REQUEST_ERROR_STATUS_CODE, REQUEST_ERROR_MESSAGE  # noqa
