468aa34394d55de362a3d0fd333e674aaded3ce7
Kat Stevens <katherine.stevens@digital.cabinet-office.gov.uk>
2018-02-16 11:01:52 +0000

    Filter Briefs by unusccessful_on and cancelled_on dates

--- dmapiclient/__init__.py
+++ dmapiclient/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '14.2.0'
+__version__ = '14.3.0'
 
 from .errors import APIError, HTTPError, InvalidResponse  # noqa
 from .errors import REQUEST_ERROR_STATUS_CODE, REQUEST_ERROR_MESSAGE  # noqa
