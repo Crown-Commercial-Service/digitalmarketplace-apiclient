e4e1c2b14fa46be26ad33d8d2bedda343036c938
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2018-03-08 12:04:09 +0000

    request id handling: switch to using request.get_onwards_request_headers if available
    however still fall back to using request.request_id failing that for
    compatibility

--- tests/test_data_api_client.py
+++ tests/test_data_api_client.py
@@ -14,27 +14,43 @@ def data_client():
 
 
 class TestDataApiClient(object):
-    def test_request_id_is_added_if_available(self, data_client, rmock, app):
+    def test_onwards_request_headers_added_if_available(self, data_client, rmock, app):
+        rmock.get("http://baseurl/_status", json={"status": "ok"}, status_code=200)
         with app.test_request_context('/'):
-            app.config['DM_REQUEST_ID_HEADER'] = 'DM-Request-Id'
-            request.request_id = 'generated'
-            rmock.get("http://baseurl/_status", json={"status": "ok"}, status_code=200)
+            # add a simple mock callable instead of using a full request implementation
+            request.get_onwards_request_headers = mock.Mock()
+            request.get_onwards_request_headers.return_value = {
+                "Douce": "bronze",
+                "Kennedy": "gold",
+            }
 
             data_client.get_status()
 
-            assert rmock.last_request.headers["DM-Request-Id"] == "generated"
+            assert rmock.last_request.headers["Douce"] == "bronze"
+            assert rmock.last_request.headers["kennedy"] == "gold"
 
-    def test_request_id_is_not_added_if_logging_is_not_loaded(self, data_client, rmock, app):
-        headers = {'DM-Request-Id': 'generated'}
-        with app.test_request_context('/', headers=headers):
-            rmock.get(
-                "http://baseurl/_status",
-                json={"status": "ok"},
-                status_code=200)
+            assert request.get_onwards_request_headers.call_args_list == [
+                # just a single, arg-less call
+                (),
+            ]
+
+    def test_onwards_request_headers_not_available(self, data_client, rmock, app):
+        rmock.get("http://baseurl/_status", json={"status": "ok"}, status_code=200)
+        with app.test_request_context('/'):
+            # really just asserting no exception arose from performing a call without get_onwards_request_headers being
+            # available
+            data_client.get_status()
+
+    def test_request_id_fallback(self, data_client, rmock, app):
+        # request.request_id is an old interface which we're still supporting here just for compatibility
+        rmock.get("http://baseurl/_status", json={"status": "ok"}, status_code=200)
+        app.config["DM_REQUEST_ID_HEADER"] = "Bar"
+        with app.test_request_context('/'):
+            request.request_id = "Ormond"
 
             data_client.get_status()
 
-            assert "DM-Request-Id" not in rmock.last_request.headers
+            assert rmock.last_request.headers["bar"] == "Ormond"
 
     def test_init_app_sets_attributes(self, data_client):
         app = mock.Mock()
