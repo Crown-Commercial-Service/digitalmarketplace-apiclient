cec8718930d92f9dd0120df8f927924af6330996
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2018-10-17 11:24:06 +0100

    Bump to version 19.7.0

--- dmapiclient/__init__.py
+++ dmapiclient/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '19.6.0'
+__version__ = '19.7.0'
 
 from .errors import APIError, HTTPError, InvalidResponse  # noqa
 from .errors import REQUEST_ERROR_STATUS_CODE, REQUEST_ERROR_MESSAGE  # noqa
