c3be694de6e76d9b703bd6d545bc10aa564df26c
Argyris Galamatis <argyris.galamatis@digital.cabinet-office.gov.uk>
2019-04-23 15:30:37 +0100

    major version change to 20 and short description in the CHANGELOG

--- dmapiclient/__init__.py
+++ dmapiclient/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '19.15.2'
+__version__ = '20.0.0'
 
 from .errors import APIError, HTTPError, InvalidResponse  # noqa
 from .errors import REQUEST_ERROR_STATUS_CODE, REQUEST_ERROR_MESSAGE  # noqa
