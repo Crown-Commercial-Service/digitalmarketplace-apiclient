921053a2c2b678633644f471f88d3f3a63879a21
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2019-08-29 09:35:39 +0100

    BaseAPIClient: propagate client_wait_for_response kwarg to all http method methods

--- dmapiclient/base.py
+++ dmapiclient/base.py
@@ -108,36 +108,36 @@ class BaseAPIClient(object):
         self._enabled = enabled
         self._timeout = timeout
 
-    def _patch(self, url, data):
-        return self._request("PATCH", url, data=data)
+    def _patch(self, url, data, *, client_wait_for_response: bool = True):
+        return self._request("PATCH", url, data=data, client_wait_for_response=client_wait_for_response)
 
-    def _patch_with_updated_by(self, url, data, user):
+    def _patch_with_updated_by(self, url, data, user, *, client_wait_for_response: bool = True):
         data = dict(data, updated_by=user)
-        return self._patch(url, data)
+        return self._patch(url, data, client_wait_for_response=client_wait_for_response)
 
-    def _put(self, url, data):
-        return self._request("PUT", url, data=data)
+    def _put(self, url, data, *, client_wait_for_response: bool = True):
+        return self._request("PUT", url, data=data, client_wait_for_response=client_wait_for_response)
 
-    def _put_with_updated_by(self, url, data, user):
+    def _put_with_updated_by(self, url, data, user, *, client_wait_for_response: bool = True):
         data = dict(data, updated_by=user)
-        return self._put(url, data)
+        return self._put(url, data, client_wait_for_response=client_wait_for_response)
 
-    def _get(self, url, params=None):
-        return self._request("GET", url, params=params)
+    def _get(self, url, params=None, *, client_wait_for_response: bool = True):
+        return self._request("GET", url, params=params, client_wait_for_response=client_wait_for_response)
 
-    def _post(self, url, data):
-        return self._request("POST", url, data=data)
+    def _post(self, url, data, *, client_wait_for_response: bool = True):
+        return self._request("POST", url, data=data, client_wait_for_response=client_wait_for_response)
 
-    def _post_with_updated_by(self, url, data, user):
+    def _post_with_updated_by(self, url, data, user, *, client_wait_for_response: bool = True):
         data = dict(data, updated_by=user)
-        return self._post(url, data)
+        return self._post(url, data, client_wait_for_response=client_wait_for_response)
 
-    def _delete(self, url, data=None):
-        return self._request("DELETE", url, data=data)
+    def _delete(self, url, data=None, *, client_wait_for_response: bool = True):
+        return self._request("DELETE", url, data=data, client_wait_for_response=client_wait_for_response)
 
-    def _delete_with_updated_by(self, url, data, user):
+    def _delete_with_updated_by(self, url, data, user, *, client_wait_for_response: bool = True):
         data = dict(data, updated_by=user)
-        return self._delete(url, data)
+        return self._delete(url, data, client_wait_for_response=client_wait_for_response)
 
     def _build_url(self, url, params):
         if not self._base_url:
