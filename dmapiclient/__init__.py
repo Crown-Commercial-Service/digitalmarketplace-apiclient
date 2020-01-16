bd9d221e196899681b00d228c926c29e68cad684
Kat Stevens <katherine.stevens@digital.cabinet-office.gov.uk>
2020-01-16 11:51:57 +0000

    Add new AuditType for updating the supplier on a service

--- dmapiclient/__init__.py
+++ dmapiclient/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '21.4.1'
+__version__ = '21.5.0'
 
 from .errors import APIError, HTTPError, InvalidResponse  # noqa
 from .errors import REQUEST_ERROR_STATUS_CODE, REQUEST_ERROR_MESSAGE  # noqa
