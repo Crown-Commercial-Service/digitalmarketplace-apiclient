219e149232a339c8958b14942966d0b67df3096c
Benjamin Gill <benjamin.gill@digital.cabinet-office.gov.uk>
2020-12-18 12:08:37 +0000

    Document responses that are sometimes paginated
    Some API endpoints are paginated and some are not. But there also exist three that may be paginated depending on the request parameters. This last category is rather confusing, so document them.

--- dmapiclient/__init__.py
+++ dmapiclient/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '21.10.0'
+__version__ = '21.10.1'
 
 from .errors import APIError, HTTPError, InvalidResponse  # noqa
 from .errors import REQUEST_ERROR_STATUS_CODE, REQUEST_ERROR_MESSAGE  # noqa
